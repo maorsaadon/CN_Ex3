3c569689964b265618afa5a22813ba276b161466
maorsaadon <105555708+maorsaadon@users.noreply.github.com>
2022-12-21 01:25:16 +0200

    Add files via upload

--- Receiver.c
+++ Receiver.c
@@ -9,7 +9,7 @@
 
 #define SERVER_PORT 5060
 #define BUFFER_SIZE 16
-#define BUNDLE 32768
+
 
 int main() {
     
@@ -20,6 +20,7 @@ int main() {
         return 1;
     }
 
+    //for reusing of port
     int enableReuse = 1;
     int ret = setsockopt(listeningSocket, SOL_SOCKET, SO_REUSEADDR, &enableReuse, sizeof(int));
     if (ret < 0) {
@@ -30,14 +31,23 @@ int main() {
     // "sockaddr_in" is the "derived" from sockaddr structure
     // used for IPv4 communication. For IPv6, use sockaddr_in6
     struct sockaddr_in serverAddress;
+
+    /*
+    fills the first 'sizeof(serverAddress)' bytes of the memory 
+    area pointed to by &serverAddress with the constant 0.
+    */
     memset(&serverAddress, 0, sizeof(serverAddress));
 
+    //address family, AF_INET(while using TCP) undighned 
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY; // any IP at this port (Address to accept any incoming messages)
+    //any IP at this port (Address to accept any incoming messages)
+    serverAddress.sin_addr.s_addr = INADDR_ANY; 
+    //change the port number from little endian => network endian(big endian): 
     serverAddress.sin_port = htons(SERVER_PORT);  // network order (makes byte order consistent)
 
     // Bind the socket to the port with any IP at this port
     int bindResult = bind(listeningSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+    //checking
     if (bindResult == -1) {
         printf("Bind failed with error code : %d", errno);
         close(listeningSocket);
@@ -48,6 +58,7 @@ int main() {
     // 500 is a Maximum size of queue connection requests
     // number of concurrent connections = 3
     int listenResult = listen(listeningSocket, 3);
+    //checking
     if (listenResult == -1) {
         printf("listen() failed with error code : %d", errno);
         close(listeningSocket);
@@ -56,57 +67,70 @@ int main() {
 
     // Accept and incoming connection
     struct sockaddr_in clientAddress;  
+
+    /*
+    fills the first 'sizeof(clientAddress)' bytes of the memory 
+    area pointed to by &clientAddress with the constant 0.
+    */
     memset(&clientAddress, 0, sizeof(clientAddress));
+
+    //saving the size of clientAddress in socklen_t variable
     socklen_t len_clientAddress = sizeof(clientAddress);
+    
+    //accept a connection on a socket
     int clientSocket = accept(listeningSocket, (struct sockaddr *) &clientAddress, &len_clientAddress);
+    //checking
     if (clientSocket == -1) {
         printf("listen failed with error code : %d", errno);
         close(listeningSocket);
         return -1;
     }else printf("A new client connection accepted\n");
 
-    int fileSize;
-    int dummy = 0;
+    int fileSize;//the file size that we receiv from the sender
+    int dummy = 0;//agreed sign
+    //receiv the file size from the sender
     recv(clientSocket, &fileSize, sizeof(int), 0);
+    //send the agreed sign to the sender
     send(clientSocket, &dummy, sizeof(int), 0);
-    
-    
 
-    long timeOfPartA[1000];
-    bzero(timeOfPartA, 1000);
-    long timeOfPartB[1000];
-    bzero(timeOfPartB, 1000);
-    long counter = 0;
+    long timeOfPartA[1000];//long array to save the run time of sending partA
+    bzero(timeOfPartA, 1000);//make a zero array
+    long timeOfPartB[1000];//long array to save the run time of sending partB
+    bzero(timeOfPartB, 1000);//make a zero array
+    long counter = 0;//present the number of sending the whole file
 
-    int running = 1;
+    int running = 1;//stop condition
     while(running)
     {
         
-        char cc_algo[BUFFER_SIZE];
+        char cc_algo[BUFFER_SIZE];//char array for changing the algorithem
         printf("Changing to cubic...\n");
-        strcpy(cc_algo, "cubic");
-        socklen_t len = strlen(cc_algo);
+        strcpy(cc_algo, "cubic");//copy the string "cubic" into cc_algo
+        socklen_t len = strlen(cc_algo);//saving the size of the str in the cc_algo in socklen_t variable
+        //checking & defult the cubic algorithem
         if (setsockopt(listeningSocket, IPPROTO_TCP, TCP_CONGESTION, cc_algo, len) == -1){
             perror("setsockopt");
             return -1;
         }
 
-        char buffer[fileSize/2];
-        int totalbytes = 0;
+        char buffer[fileSize/2];//char array for receiving the half of the file
+        int totalbytes = 0;//present the bytes that have been received
 
         printf("Waiting for part A...\n");
 
-        struct timeval current_time;
-        gettimeofday(&current_time,NULL);
-        long before_partA_sec = current_time.tv_sec;
-        long before_partA_mic = current_time.tv_usec;
-        long total_time_before_partA = before_partA_sec*1000000 + before_partA_mic;
+        struct timeval current_time;//struct for saving current time
+        gettimeofday(&current_time,NULL);//saving the current time
+        long before_partA_sec = current_time.tv_sec;//time in second
+        long before_partA_mic = current_time.tv_usec;//time in microsecond
+        long total_time_before_partA = before_partA_sec*1000000 + before_partA_mic;//total time befor
 
+        
         while (totalbytes < (fileSize/2))
         {
+            //receive the first part of the file
             int bytesgot = recv(clientSocket, buffer+totalbytes, sizeof(char), 0);
             totalbytes += bytesgot;
-
+            //checking
             if (bytesgot == 0){
                 printf("Connection with sender closed, exiting...\n");
                 running = 0;
@@ -114,28 +138,29 @@ int main() {
             }
         }
 
-        if (running == 0) break;
+        if (running == 0) break; //quit the program if somthing wrong
 
-        gettimeofday(&current_time,NULL);
-        long after_partA_sec = current_time.tv_sec;
-        long after_partA_mic = current_time.tv_usec;
-        long total_time_after_partA = after_partA_sec*1000000 + after_partA_mic;
-        long total_time_partA = total_time_after_partA - total_time_before_partA;
-        timeOfPartA[counter]=total_time_partA;
+        gettimeofday(&current_time,NULL);//saving the current time
+        long after_partA_sec = current_time.tv_sec;//time in second
+        long after_partA_mic = current_time.tv_usec;//time in microsecond
+        long total_time_after_partA = after_partA_sec*1000000 + after_partA_mic;//total time after
+        long total_time_partA = total_time_after_partA - total_time_before_partA;//tatal time that took part A
+        timeOfPartA[counter]=total_time_partA;//saving the time in the array
 
         printf("Got part A\n");
 
         printf("Sending authntication check\n");
 
-        int xor = 2421 ^ 7494;
-        send(clientSocket, &xor, sizeof(int), 0);
+        int receiver_xor = 2421 ^ 7494; //the receiver xor
+        send(clientSocket, &receiver_xor, sizeof(int), 0);//the receiver send his xor to the sender for the authentication
 
         printf("Authontication sent\n");
 
         printf("Changeing to reno..\n");
 
-        strcpy(cc_algo, "reno");
-        len = strlen(cc_algo);
+        strcpy(cc_algo, "reno");//copy the string "reno" into cc_algo
+        len = strlen(cc_algo);//saving the size of the str in the cc_algo in socklen_t variable
+        //checking & defult the cubic algorithem
         if (setsockopt(listeningSocket, IPPROTO_TCP, TCP_CONGESTION, cc_algo, len) == -1){
             perror("setsockopt");
             return -1;
@@ -145,17 +170,18 @@ int main() {
 
         totalbytes = 0;
 
-        gettimeofday(&current_time,NULL);
-        long before_partB_sec = current_time.tv_sec;
-        long before_partB_mic = current_time.tv_usec;
-        long total_time_before_partB = before_partB_sec*1000000 + before_partB_mic;
+        gettimeofday(&current_time,NULL);//saving the current time
+        long before_partB_sec = current_time.tv_sec;//time in second
+        long before_partB_mic = current_time.tv_usec;//time in microsecond
+        long total_time_before_partB = before_partB_sec*1000000 + before_partB_mic;//total time befor
 
         
         while (totalbytes < (fileSize/2))
         {
+            //receive the second part of the file
             int bytesgot = recv(clientSocket, buffer+totalbytes, sizeof(char), 0);
             totalbytes += bytesgot;
-
+            //checking
             if (bytesgot == 0){
                 printf("Connection with sender closed, exiting...\n");
                 running = 0;
@@ -163,36 +189,37 @@ int main() {
             }
         }
 
-        if (running == 0) break;
+        if (running == 0) break;//quit the program if somthing wrong
         
-        gettimeofday(&current_time,NULL);
-        long after_partB_sec = current_time.tv_sec;
-        long after_partB_mic = current_time.tv_usec;
-        long total_time_after_partB = after_partB_sec*1000000 + after_partB_mic;
-        
-        long total_time_partB = total_time_after_partB - total_time_before_partB;
-        timeOfPartB[counter]=total_time_partB;
-        counter++;
+        gettimeofday(&current_time,NULL);//saving the current time
+        long after_partB_sec = current_time.tv_sec;//time in second
+        long after_partB_mic = current_time.tv_usec;//time in microsecond
+        long total_time_after_partB = after_partB_sec*1000000 + after_partB_mic;//total time after
+        long total_time_partB = total_time_after_partB - total_time_before_partB;//tatal time that took part B
+        timeOfPartB[counter]=total_time_partB;//saving the time in the array
+        counter++;//add 1 for the counter after we finished one receinging of the whole file
 
         printf("Got part B\n");
-        recv(clientSocket, &dummy, sizeof(int), 0);
+        recv(clientSocket, &dummy, sizeof(int), 0);//receiving from the sender the agreed sign
     }
 
     printf("Exit\n");
-    close(clientSocket);
-    close(listeningSocket);
+    close(clientSocket);//close the clientSocket
+    close(listeningSocket);//listeningSocket
 
-    long total_time_of_A = 0;
-    long total_time_of_B = 0;
+    long total_time_of_A = 0;//present the tatal time for receiving part A(for all times)
+    long total_time_of_B = 0;//present the tatal time for receiving part A(for all times)
     
     for(int i=0; i<counter; i++){
         printf("Run time of part A, number %d : (%ld second,%ld microseconed)\n",i+1 ,(timeOfPartA[i]/1000000),(timeOfPartA[i]%1000000));
         printf("Run time of part B, number %d : (%ld second,%ld microseconed)\n",i+1,(timeOfPartB[i]/1000000),(timeOfPartB[i]%1000000));
 
+        //sum the time of each receiving (part A/B)
         total_time_of_A += timeOfPartA[i];
         total_time_of_B += timeOfPartB[i];
     }
     
+    //calculate the average
     long average_time_of_A = (total_time_of_A/counter);
     long average_time_of_B = (total_time_of_B/counter);
 
--- Sender.c
+++ Sender.c
@@ -15,39 +15,52 @@
 
 int main() {
     
-    //open the file
+    //open a file
     FILE *f = fopen(FILE_TO_SEND, "r");
+    //checking
     if (f == NULL){
         fprintf(stderr, "Error opening file");
         return 1;
     }
-    //calculate file size
+    //calculate the file size
     fseek(f, 0, SEEK_END);
     int fileSize = (int) ftell(f);
     fseek(f, 0, SEEK_SET);
 
+    //put the file in chat array
     char filedata[fileSize];
     fread(filedata, sizeof(char), fileSize, f);
+    
+    //close the file
     fclose(f);
 
     //open a socket
     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //checking
     if (sock == -1) {
         printf("Could not create socket : %d", errno);
         return -1;
     } else printf("New socket opened\n");
 
-
-    //"sockaddr_in" is the "derived" from sockaddr structure
-    //used for IPv4 communication. For IPv6, use sockaddr_in6
+    /*
+    "sockaddr_in" is the "derived" from sockaddr structure
+    used for IPv4 communication. For IPv6, use sockaddr_in6
+    */
     struct sockaddr_in serverAddress;
+
+    /*
+    fills the first 'sizeof(serverAddress)' bytes of the memory 
+    area pointed to by &serverAddress with the constant 0.
+    */
     memset(&serverAddress, 0, sizeof(serverAddress));
 
+    //address family, AF_INET(while using TCP) undighned 
     serverAddress.sin_family = AF_INET;
-    //little endian => network endian (big endian):
+    //change the port number from little endian => network endian(big endian): 
     serverAddress.sin_port = htons(SERVER_PORT);
     //convert IPv4 and IPv6 addresses from text to binary form
     int rval = inet_pton(AF_INET, (const char *) SERVER_IP_ADDRESS, &serverAddress.sin_addr);
+    //checking
     if (rval <= 0){
         printf("inet_pton() failed");
         return -1;
@@ -55,22 +68,24 @@ int main() {
 
     // Make a connection to the server with socket SendingSocket.
     int connectResult = connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+    //checking
     if (connectResult == -1){
         printf("connect() failed with error code : %d", errno);
         close(sock);
         return -1;
     }else printf("connected to server\n");
 
-    int dummy = 0;
+    int dummy = 0;//agreed sign
     
-    send(sock, &fileSize, sizeof(int), 0);
-    recv(sock, &dummy, sizeof(int), 0);
+    send(sock, &fileSize, sizeof(int), 0); //send the filesize to the receiver
+    recv(sock, &dummy, sizeof(int), 0);//waiting for receiver to get the agreed sign
     
     while (1)
     {
-        char cc_algo[BUFFER_SIZE];
-        strcpy(cc_algo, "cubic");
-        socklen_t len = strlen(cc_algo);
+        char cc_algo[BUFFER_SIZE];//char array for changing the algorithem
+        strcpy(cc_algo, "cubic");//copy the string "cubic" into cc_algo
+        socklen_t len = strlen(cc_algo);//saving the size of the str in the cc_algo in socklen_t variable
+        //checking & defult the cubic algorithem
         if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, cc_algo, len) == -1){
             perror("setsockopt");
             return -1;
@@ -78,23 +93,28 @@ int main() {
 
         printf("Sending part A using cubic CC algorithm\n");
 
-        long bytesSent = -1;
+        long bytesSent = -1;//present the number of bytes that send
 
+        //send the first half of the file
         bytesSent = send(sock, filedata, (fileSize/2), 0);
+        //checking
         if (bytesSent == -1) printf("send() failed with error code : %d", errno);
         else if (bytesSent == 0) printf("peer has closed the TCP connection prior to send().\n");
         else if (bytesSent < (fileSize/2)) printf("sent only %ld bytes from the required %d.\n", bytesSent, (fileSize/2));
         else printf("Sent %ld bytes\n", bytesSent);
         
-        int xor = 2421 ^ 7494;
-        int get_xor;
+        int sender_xor = 2421 ^ 7494; //the sender xor for the authentication
+        int receiver_xor;//the xor that the sender get from the receiver
 
-        recv(sock, &get_xor, sizeof(int), 0);
-        if (get_xor == xor) printf("Part A was successfully sent\n");
+        //receiv the xor from the receiver
+        recv(sock, &receiver_xor, sizeof(int), 0);
+        //checking
+        if (receiver_xor == sender_xor) printf("Part A was successfully sent\n");
         else printf("client: something went wrong\n");
 
-        strcpy(cc_algo, "reno");
-        len = strlen(cc_algo);
+        strcpy(cc_algo, "reno");//copy the string "reno" into cc_algo
+        len = strlen(cc_algo);//saving the size of the str in the cc_algo in socklen_t variable
+        //checking & defult the cubic algorithem
         if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, cc_algo, len) != 0){
             perror("setsockopt");
             return -1;
@@ -102,7 +122,9 @@ int main() {
 
         printf("Sending part B using reno CC algorithm\n");
 
+        //send the second half of the file
         bytesSent = send(sock, filedata+(fileSize/2)-1, (fileSize/2), 0);
+        //checking
         if (bytesSent == -1) printf("send() failed with error code : %d", errno);
         else if (bytesSent == 0) printf("peer has closed the TCP connection prior to send().\n");
         else if (bytesSent < (fileSize/2)) printf("sent only %ld bytes from the required %d.\n", bytesSent, (fileSize/2));
@@ -110,12 +132,13 @@ int main() {
         
         printf("Part B was successfully sent\n");
         
-        char choose;
+        char choose; //the user decision
         printf("Send again? Y = yes, N = no\n");
         scanf(" %c", &choose);
 
-        send(sock, &dummy, sizeof(int), 0);
+        send(sock, &dummy, sizeof(int), 0);//sending the receiver the agreed sign
 
+        //if the user want to close the program --> close the socket and break
         if (choose == 'N'){
             close(sock);
             break;
